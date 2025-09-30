194d84ff8fc994df5dd43b78663f95a4bd4d6804
bab1bar <babarjani042@gmail.com>
2025-09-30 12:46:25 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.getElementById("green").onclick = function() {
+    document.body.style.backgroundColor = "green";
+}
+
+document.getElementById("red").onclick = function() {
+    document.body.style.backgroundColor = "red";
+}
+
+document.getElementById("blue").onclick = function() {
+    document.body.style.backgroundColor = "blue";
+}
+
+document.getElementById("yellow").onclick = function() {
+    document.body.style.backgroundColor = "yellow";
+}
+
+document.getElementById("cyan").onclick = function() {
+    document.body.style.backgroundColor = "cyan";
+}       
+
+document.getElementById("purple").onclick = function() {
+    document.body.style.backgroundColor = "purple";
+}
+
+document.getElementById("orange").onclick = function() {
+    document.body.style.backgroundColor = "orange";
+}   
+
+
